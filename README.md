018d4a56cdde78e89fb7353e17cd1e37d9f25887
MoazHassain <moazhassain@gmail.com>
2022-09-13 00:25:25 +0600

    initial commit and theme setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# weather_app
+
+A Flutter project for weather with weather api.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources for this Flutter project:
+
+- [To get weather api](https://openweathermap.org/api)
+- [To test and document the api](https://restless-trinity-774240.postman.co/)
+
---
+++ lib/theme/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const Color clrBlack000 = Color(0xff010710);
+const Color clrBlack100 = Color(0xff717578);
+const Color clrWhite000 = Color(0xffFBFBFB);
+const Color clrWhite100 = Color(0xffF4F4F4);
+const Color clrPrimary = Color(0xffF58A5B);
+
+const FontWeight fontMedium = FontWeight.w500;
+myfont(
+  double? size, [
+  Color? clr,
+  FontWeight? fw,
+]) {
+  return TextStyle(
+    fontSize: size,
+    color: clr == null ? clrBlack000 : clr,
+    fontWeight: fw == null ? fontMedium : fw,
+  );
+}
